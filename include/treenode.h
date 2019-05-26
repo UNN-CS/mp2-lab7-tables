ed74bbf910c4499f0eb057895ab1fd5c5da19d2d
Arseniy Obolenskiy <gooddoog@student.su>
2019-05-26 11:01:56 +0300

    Implemented all methonds and added tests

---
+++ include/treenode.h
@@ -0,0 +1,21 @@
+#ifndef TREENODE_H_
+#define TREENODE_H_
+#include "tabrecord.h"
+
+class TTreeNode;
+typedef TTreeNode *PTTreeNode;
+
+class TTreeNode: public TTabRecord {
+ protected:
+    PTTreeNode pLeft, pRight; // указатели на поддеревья
+ public:
+    TTreeNode(TKey k = "", PTDatValue pVal = nullptr, PTTreeNode pL = nullptr,
+            PTTreeNode pR = nullptr): TTabRecord(k,pVal), pLeft(pL), pRight(pR) {};
+    PTTreeNode GetLeft() const; // указатель на левое поддерево
+    PTTreeNode GetRight() const; // указатель на правое поддерево
+    virtual TDatValue *GetCopy();  // изготовить копию
+    friend class TTreeTable;
+    friend class TBalanceTree;
+};
+
+#endif // TREENODE_H_
