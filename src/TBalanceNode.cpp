00b3889abdfaa9be34da990d0c3dad80b20b3750
MacLin123 <wallsoftheb77@gmail.com>
2019-05-31 23:21:20 +0300

    Done

---
+++ src/TBalanceNode.cpp
@@ -0,0 +1,18 @@
+#include "TBalanceNode.h"
+
+TDatValue * TBalanceNode::GetCopy()
+{
+    TBalanceNode* tmp = new TBalanceNode(Key,pValue,nullptr,nullptr,BalOk);
+    return tmp;
+}
+
+int TBalanceNode::GetBalance(void) const
+{
+    return Balance;
+}
+
+void TBalanceNode::SetBalance(int bal)
+{
+    Balance = bal;
+}
+
